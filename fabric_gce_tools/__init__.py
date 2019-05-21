f586a64ceeb55946add8e508cb4943cfce5777a1
wg <wxkxg@protonmail.com>
2019-05-21 10:12:36 -0700

    run 2to3 against source code

--- fabric_gce_tools/__init__.py
+++ fabric_gce_tools/__init__.py
@@ -20,7 +20,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-from __future__ import absolute_import
+
 import subprocess
 import os.path
 import time
